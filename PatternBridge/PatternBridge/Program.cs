c2e458df4551675e61929de6fcb43a9db81c1808
us <us@08-01>
2017-12-04 20:36:51 +0200

    build classes

--- PatternBridge/PatternBridge/Program.cs
+++ PatternBridge/PatternBridge/Program.cs
@@ -12,86 +12,107 @@ namespace PatternBridge
     }
     public abstract class Logger
     {
-        protected LoggerImpl implementator;
+        public LoggerImpl implementator;
         public Logger(LoggerImpl implementator)
         {
             this.implementator = implementator;
         }
 
-        public virtual void Debug() => implementator.Debug();
-
-        public virtual void Error() => implementator.Error();
-
-        public virtual void Info() => implementator.Info();
-
-        public virtual void Verbose() => implementator.Verbose();
-
-        public virtual void Warning() => implementator.Warning();
+        public virtual void Debug(string msg) {}
+        public virtual void Warning(string msg) { }
+        public virtual void Info(string msg) { }
+        public virtual void Error(string msg) { }
+        public virtual void Verbose(string msg) { }
     }
 
-    public abstract class LoggerImpl
+    public interface LoggerImpl
     {
-        public abstract void Debug();
-        public abstract void Error();
-        public abstract void Warning();
-        public abstract void Info();
-        public abstract void Verbose();
+        void ConsoleLog(string msg, Level lvl);
+        void FileLog(string msg, Level lvl);
+        void SocketLog(string msg, Level lvl);
     }
 
     public class ConsoleLogger : Logger
     {
         public ConsoleLogger(LoggerImpl implementator) 
             : base(implementator) { }
-        public override void Debug() => implementator.Debug();
+        public override void Debug(string msg) => implementator.ConsoleLog(msg, Level.Debug);
+        public override void Warning(string msg) => implementator.ConsoleLog(msg, Level.Warning);
+        public override void Info(string msg) => implementator.ConsoleLog(msg, Level.Info);
+        public override void Error(string msg) => implementator.ConsoleLog(msg, Level.Error);
+        public override void Verbose(string msg) => implementator.ConsoleLog(msg, Level.Verbose);
     }
-    //public class SocketLogger: Logger
-    //{
 
-    //}
-    //public class FileLogger: Logger
-    //{
+    public class FileLogger : Logger
+    {
+        public FileLogger(LoggerImpl implementator) 
+            : base(implementator) { }
+        public override void Debug(string msg) => implementator.FileLog(msg, Level.Debug);
+        public override void Warning(string msg) => implementator.FileLog(msg, Level.Warning);
+        public override void Info(string msg) => implementator.FileLog(msg, Level.Info);
+        public override void Error(string msg) => implementator.FileLog(msg, Level.Error);
+        public override void Verbose(string msg) => implementator.FileLog(msg, Level.Verbose);
+    }
 
-    //}
-
+    public class SocketLogger : Logger
+    {
+        public SocketLogger(LoggerImpl implementator)
+            : base(implementator) { }
+        public override void Debug(string msg) => implementator.SocketLog(msg, Level.Debug);
+        public override void Warning(string msg) => implementator.SocketLog(msg, Level.Warning);
+        public override void Info(string msg) => implementator.SocketLog(msg, Level.Info);
+        public override void Error(string msg) => implementator.SocketLog(msg, Level.Error);
+        public override void Verbose(string msg) => implementator.SocketLog(msg, Level.Verbose);
+    }
 
     public class ST_LoggerImpl : LoggerImpl
     {
-        public override void Debug()
+        public void ConsoleLog(string msg, Level lvl)
         {
-            Console.WriteLine("Debug");
+            Console.WriteLine("ST Console log");
         }
-
-        public override void Error()
+        public void FileLog(string msg, Level lvl)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("ST File log");
         }
-
-        public override void Info()
+        public void SocketLog(string msg, Level lvl)
         {
-            throw new NotImplementedException();
-        }
-
-        public override void Verbose()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Warning()
-        {
-            throw new NotImplementedException();
+            Console.WriteLine("ST Socket log");
         }
     }
-    public class MT_LoggerImpl
-    {
 
+    public class MT_LoggerImpl : LoggerImpl
+    {
+        public void ConsoleLog(string msg, Level lvl)
+        {
+            Console.WriteLine("MT Console log");
+        }
+        public void FileLog(string msg, Level lvl)
+        {
+            Console.WriteLine("MT File log");
+        }
+        public void SocketLog(string msg, Level lvl)
+        {
+            Console.WriteLine("MT Socket log");
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Logger logger = new ConsoleLogger(new ST_LoggerImpl());
-            logger.Debug();
+            Logger consoleLogger = new ConsoleLogger(new ST_LoggerImpl());
+            consoleLogger.Debug("");
+
+            Logger fileLogger = new FileLogger(new ST_LoggerImpl());
+            fileLogger.Debug("");
+
+            Logger socketLogger = new SocketLogger(new ST_LoggerImpl());
+            socketLogger.Debug("");
+
+
+
+            Console.Read();
         }
     }
 }
