36dae1b91c70574ad799092bc7dcb24696169552
Любимов Витаха <l0g_d0g@bk.ru>
2017-12-08 08:15:37 +0200

    updated

--- ChainOf Responsibility/ChainOf Responsibility/Program.cs
+++ ChainOf Responsibility/ChainOf Responsibility/Program.cs
@@ -12,7 +12,7 @@ namespace ChainOf_Responsibility
     }
     interface INote
     {
-        INote SeniorNote { get; set; }
+        INote JuniorNote { get; set; }
         CurrencyType Currency { get; set; }
         int Value { get; set; }
 
@@ -20,14 +20,14 @@ namespace ChainOf_Responsibility
 
     class Note: INote
     {
-        public INote SeniorNote { get; set; }
+        public INote JuniorNote { get; set; }
         public CurrencyType Currency { get; set; }
         public int Value { get; set; }
         public Note(CurrencyType currency, int value, INote seniorNote)
         {
             Currency = currency;
             Value = value;
-            SeniorNote = seniorNote;
+            JuniorNote = seniorNote;
         }
     }
 
@@ -37,21 +37,23 @@ namespace ChainOf_Responsibility
         /// INote - купюра
         /// int - количество купюр
         /// </summary>
-        private Dictionary<INote, int> currency = new Dictionary<INote, int>();
-        private readonly int maxNote = 500;     //самая большая купюра в банкомате
-        private readonly int minNote = 500;     //самая маленькая купюра в банкомате
+        private Dictionary<INote, int> currency = new Dictionary<INote, int>(); //купюры в банкомате
+        private readonly int maxNote = 500;             //самая большая купюра в банкомате
+        private readonly int minNote = 5;             //самая маленькая купюра в банкомате
         private readonly int initionalQuantity = 1000;  //изначальное количество каждой из купюр
         public ATM()
         {
 
+
             /* Инициализация купюр в банкомате */
-            INote note500 = new Note(CurrencyType.UAH, 500, null);
-            INote note200 = new Note(CurrencyType.UAH, 200, note500);
-            INote note100 = new Note(CurrencyType.UAH, 100, note200);
-            INote note50 = new Note(CurrencyType.UAH, 500, note100);
-            INote note20 = new Note(CurrencyType.UAH, 20, note50);
-            INote note10 = new Note(CurrencyType.UAH, 10, note20);
-            INote note5 = new Note(CurrencyType.UAH, 5, note10);
+            INote note5 = new Note(CurrencyType.UAH, 5, null);
+            INote note10 = new Note(CurrencyType.UAH, 10, note5);
+            INote note20 = new Note(CurrencyType.UAH, 20, note10);
+            INote note50 = new Note(CurrencyType.UAH, 500, note20);
+            INote note100 = new Note(CurrencyType.UAH, 100, note50);
+            INote note200 = new Note(CurrencyType.UAH, 200, note100);
+            INote note500 = new Note(CurrencyType.UAH, 500, note200);
+            
             
             currency.Add(note500, 0);
             currency.Add(note200, initionalQuantity);
@@ -59,17 +61,29 @@ namespace ChainOf_Responsibility
             currency.Add(note50, initionalQuantity);
             currency.Add(note20, initionalQuantity);
             currency.Add(note10, initionalQuantity);
-            currency.Add(note5, initionalQuantity);
+            currency.Add(note5, 0);
         }
         //Выдача наличных
-        public int CashWithdrawal(int money)
+        public bool CashWithdrawal(int money)
         {
             //Выбор наибольшей доступной купюры, которая есть в наличии
             var pair = currency.Where(item => item.Key.Value <= money && item.Value > 0).FirstOrDefault();
             int total = money;
 
+            if (total < minNote || pair.Key is null) return false;
 
-            return 0;
+            while(total != 0)
+            {
+                if (pair.Key.Value == minNote && pair.Value == 0) break;
+                if (pair.Value > 0 && total > pair.Key.Value)
+                {
+                    total -= pair.Key.Value;
+                    currency[pair.Key] = pair.Value - 1;
+                    pair = currency.Where(item => item.Key.Value == pair.Key.Value).First();
+                }
+                else CashWithdrawal(total);
+            }
+            return true;
         }
     }
 
@@ -79,8 +93,7 @@ namespace ChainOf_Responsibility
         {
             ATM atm = new ATM();
 
-            atm.CashWithdrawal(2000);
-
+            bool isCashWithdrawal = atm.CashWithdrawal(153);
 
             Console.Read();
         }
